54cdbd2647692de148b90d6a71f9ffc83194580a
arimu <arimurahiroaki40@gmail.com>
2025-01-24 10:26:06 +0900

    フロントエンド側を開発

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { PostType } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,10 +7,12 @@ async function fetchAllBlogs() {
     cache: "no-store",
   });
   const data = await res.json();
-  return data;
+  return data.posts;
 }
 
-export default function Home() {
+export default async function Home() {
+  const posts = await fetchAllBlogs();
+
   return (
     <div>
       <main className="w-full h-full">
@@ -29,29 +32,34 @@ export default function Home() {
         </div>
 
         <div className="w-full flex flex-col justify-center items-center">
-          <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center">
-            <div className="flex items-center my-3">
-              <div className="mr-auto">
-                <h2 className="mr-auto font-semibold">First Post</h2>
+          {posts.map((post: PostType) => (
+            <div
+              key={post.id}
+              className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center"
+            >
+              <div className="flex items-center my-3">
+                <div className="mr-auto">
+                  <h2 className="mr-auto font-semibold">{post.title}</h2>
+                </div>
+                <Link
+                  href={`/blog/edit/${post.id}`}
+                  className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
+                >
+                  編集
+                </Link>
               </div>
-              <Link
-                href={`/blog/edit/1`}
-                className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
-              >
-                編集
-              </Link>
-            </div>
 
-            <div className="mr-auto my-1">
-              <blockquote className="font-bold text-slate-700">
-                2023/8/17
-              </blockquote>
-            </div>
+              <div className="mr-auto my-1">
+                <blockquote className="font-bold text-slate-700">
+                  {new Date(post.date).toDateString()}
+                </blockquote>
+              </div>
 
-            <div className="mr-auto my-1">
-              <h2>First Post Description</h2>
+              <div className="mr-auto my-1">
+                <h2>{post.description}</h2>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </main>
       ;
